9975b9006f2b4eeef4a39cd32404fe3bf8cf599f
Tyler <tyler.woods44@gmail.com>
2015-03-04 21:28:05 -0500

    Fixed for loop in main.js

--- project-4/views/js/main.js
+++ project-4/views/js/main.js
@@ -450,10 +450,10 @@ var resizePizzas = function(size) {
 
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
-    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
-      var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
-      var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
-      document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
+    for (var i = 0; i < document.getElementByClassName(".randomPizzaContainer").length; i++) {
+      var dx = determineDx(document.getElementByClassName(".randomPizzaContainer")[i], size);
+      var newwidth = (document.getElementByClassName(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
+      document.getElementByClassName(".randomPizzaContainer")[i].style.width = newwidth;
     }
   }
 
