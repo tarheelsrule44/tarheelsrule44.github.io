4d33770b3db8e853ac80b6874af6bc42e0ce618b
Tyler <tyler.woods44@gmail.com>
2015-03-04 21:45:27 -0500

    please?

--- project-4/views/js/main.js
+++ project-4/views/js/main.js
@@ -503,8 +503,8 @@ function updatePositions() {
   window.performance.mark("mark_start_frame");
 
   var items = document.querySelectorAll('.mover');
-  for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+  var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+  for (var i = 0; i < items.length; i++) {    
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
