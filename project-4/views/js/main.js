b27d414ad45d38aa47d1f6b072fb8f73f8678bd8
Tyler <tyler.woods44@gmail.com>
2015-03-04 21:43:57 -0500

    Please work

--- project-4/views/js/main.js
+++ project-4/views/js/main.js
@@ -469,9 +469,8 @@ var resizePizzas = function(size) {
 window.performance.mark("mark_start_generating"); // collect timing data
 
 // This for-loop actually creates and appends all of the pizzas when the page loads
-
-
-for (var i = 2; i < 100; i++) {  
+var pizzasDiv = document.getElementById("randomPizzas");
+for (var i = 2; i < 100; i++) { 
   pizzasDiv.appendChild(pizzaElementGenerator(i));
 }
 
