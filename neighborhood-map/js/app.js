011da9a0f97cc602da0bb6acc255a92da409adb1
Tyler <tyler.woods44@gmail.com>
2015-03-23 12:52:56 -0400

    fixed things

--- neighborhood-map/js/app.js
+++ neighborhood-map/js/app.js
@@ -3,17 +3,138 @@ function appViewModel() {
   var map;
   var service;
   var infowindow;
-  var lat = 35.908759;
-  var lng = -79.048100;
-  var chapelHill = new google.maps.LatLng(lat, lng);
-  var markersArray = [];
-  var marker;
+  var lat = '';
+  var lng = '';
+  var chapelHill = new google.maps.LatLng(35.908759, -79.048100);
+  var markersArray = [];  
 
   // array to hold info for knockout
   self.allPlaces = ko.observableArray([]);
 
   // string to hold foursquare information
   self.foursquareInfo = '';
+
+  // Finds the center of the map to get lat and lng values
+  function computeCenter() {
+    var latAndLng = map.getCenter();
+      lat = latAndLng.lat();
+      lng = latAndLng.lng(); 
+  }
+
+  
+  /*
+  Loads the map as well as position the search bar and list.  On a search, clearOverlays removes all markers already on the map and removes all info in allPlaces.  Then, once a search is complete, populates more markers and sends the info to getAllPlaces to populate allPlaces again.
+  */
+  function initialize() {
+    map = new google.maps.Map(document.getElementById('map-canvas'), {
+    center: chapelHill,    
+    });
+    getPlaces();
+    computeCenter();       
+
+    var list = (document.getElementById('list'));
+    map.controls[google.maps.ControlPosition.RIGHT_CENTER].push(list);
+    var input = (document.getElementById('pac-input'));
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+    var searchBox = new google.maps.places.SearchBox(
+      (input));
+    google.maps.event.addListener(searchBox, 'places_changed', function() {
+      var places = searchBox.getPlaces();
+      clearOverlays();
+      self.allPlaces.removeAll();
+      var bounds = new google.maps.LatLngBounds();  
+
+
+      for(var i=0, place; i<10; i++){
+        if (places[i] !== undefined){
+          place = places[i];
+
+          getAllPlaces(place);
+          createMarker(place);
+          bounds.extend(place.geometry.location);          
+        }        
+      } 
+      map.fitBounds(bounds); 
+      computeCenter();                
+    });
+    google.maps.event.addListener(map, 'bounds_changed', function(){
+      var bounds = map.getBounds();
+      searchBox.setBounds(bounds);
+    });   
+    // Handles an event where Google Maps taks too long to load
+    var timer = window.setTimeout(failedToLoad, 5000);
+    google.maps.event.addListener(map, 'tilesloaded', function() {
+      window.clearTimeout(timer);
+    });
+  }
+  // Will let the user know when Google Maps fails to load.
+  function failedToLoad() {
+    $('#map-canvas').html("Google Maps Failed to Load");
+  }
+
+  /*
+  Function to pre-populate the map with place types.  nearbySearch retuns up to 20 places.
+  */
+  function getPlaces() {
+    var request = {
+      location: chapelHill,
+      radius: 600,
+      types: ['restaurant', 'bar', 'cafe', 'food']
+    };
+
+    infowindow = new google.maps.InfoWindow();
+    service = new google.maps.places.PlacesService(map);
+    service.nearbySearch(request, callback);    
+  }
+
+  /*
+  Gets the callback from Google and creates a marker for each place.  Sends info to getAllPlaces.
+  */
+  function callback(results, status){
+    if (status == google.maps.places.PlacesServiceStatus.OK){
+      bounds = new google.maps.LatLngBounds();
+      results.forEach(function (place){
+        place.marker = createMarker(place);
+        bounds.extend(new google.maps.LatLng(
+          place.geometry.location.lat(),
+          place.geometry.location.lng()));
+      });
+      map.fitBounds(bounds);
+      results.forEach(getAllPlaces);                 
+    }
+  }
+
+  /*
+  Function to create a marker at each place.  This is called on load of the map with the pre-populated list, and also after each search.  Also sets the content of each place's infowindow.
+  */
+  function createMarker(place) {
+    var marker = new google.maps.Marker({
+      map: map,
+      icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
+      name: place.name.toLowerCase(),
+      position: place.geometry.location,
+      place_id: place.place_id,
+      animation: google.maps.Animation.DROP
+    });    
+    var address;
+    if (place.vicinity !== undefined) {
+      address = place.vicinity;
+    } else if (place.formatted_address !== undefined) {
+      address = place.formatted_address;
+    }       
+    var contentString = '<div style="font-weight: bold">' + place.name + '</div><div>' + address + '</div>' + self.foursquareInfo ;
+
+    google.maps.event.addListener(marker, 'click', function() {      
+      infowindow.setContent(contentString);      
+      infowindow.open(map, this);
+      map.panTo(marker.position); 
+      marker.setAnimation(google.maps.Animation.BOUNCE);
+      setTimeout(function(){marker.setAnimation(null);}, 1450);
+    });
+
+    markersArray.push(marker);
+    return marker;
+  }
 
   // Foursquare Credentials
   var clientID = 'UVSLUM00CXLUB1P0UKPJSLDTG0VVYQ2E20W1C045PBU1OJNZ';
@@ -26,8 +147,7 @@ function appViewModel() {
     $.getJSON(foursquareURL)
       .done(function(response) {
         self.foursquareInfo = '<p>Foursquare:<br>';
-        var venue = response.response.venues[0];
-        var venueID = venue.id;   
+        var venue = response.response.venues[0];         
         // Name       
         var venueName = venue.name;
             if (venueName !== null && venueName !== undefined) {
@@ -96,118 +216,8 @@ function appViewModel() {
     myPlace.address = address;
     
     self.allPlaces.push(myPlace);                
-  };
+  }
 
-  /*
-  Loads the map as well as position the search bar and list.  On a search, clearOverlays removes all markers already on the map and removes all info in allPlaces.  Then, once a search is complete, populates more markers and sends the info to getAllPlaces to populate allPlaces again.
-  */
-  function initialize() {
-    map = new google.maps.Map(document.getElementById('map-canvas'), {
-    center: chapelHill,    
-    });
-    getPlaces();
-
-    var list = (document.getElementById('list'));
-    map.controls[google.maps.ControlPosition.RIGHT_CENTER].push(list);
-    var input = (document.getElementById('pac-input'));
-    map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-    var searchBox = new google.maps.places.SearchBox(
-      (input));
-    google.maps.event.addListener(searchBox, 'places_changed', function() {
-      var places = searchBox.getPlaces();
-      clearOverlays();
-      self.allPlaces.removeAll();
-      var bounds = new google.maps.LatLngBounds();
-
-      for(var i=0, place; i<10; i++){
-        if (places[i] !== undefined){
-          place = places[i];
-
-          getAllPlaces(place);
-          createMarker(place);
-          bounds.extend(place.geometry.location);          
-        }        
-      } 
-      map.fitBounds(bounds);     
-    });
-    google.maps.event.addListener(map, 'bounds_changed', function(){
-      var bounds = map.getBounds();
-      searchBox.setBounds(bounds);
-    });   
-    // Handles an event where Google Maps taks too long to load
-    var timer = window.setTimeout(failedToLoad, 4000);
-    google.maps.event.addListener(map, 'tilesloaded', function() {
-      window.clearTimeout(timer);
-    })
-  };
-  // Will let the user know when Google Maps fails to load.
-  function failedToLoad() {
-    $('#map-canvas').html("Google Maps Failed to Load");
-  };
-
-  /*
-  Function to pre-populate the map with place types.  nearbySearch retuns up to 20 places.
-  */
-  function getPlaces() {
-    var request = {
-      location: chapelHill,
-      radius: 600,
-      types: ['restaurant', 'bar', 'cafe', 'food']
-    };
-
-    infowindow = new google.maps.InfoWindow();
-    service = new google.maps.places.PlacesService(map);
-    service.nearbySearch(request, callback);    
-  };
-
-  /*
-  Gets the callback from Google and creates a marker for each place.  Sends info to getAllPlaces.
-  */
-  function callback(results, status){
-    if (status == google.maps.places.PlacesServiceStatus.OK){
-      bounds = new google.maps.LatLngBounds();
-      results.forEach(function (place){
-        place.marker = createMarker(place);
-        bounds.extend(new google.maps.LatLng(
-          place.geometry.location.lat(),
-          place.geometry.location.lng()));
-      })
-      map.fitBounds(bounds);
-      results.forEach(getAllPlaces);                 
-    }
-  };
-
-  /*
-  Function to create a marker at each place.  This is called on load of the map with the pre-populated list, and also after each search.  Also sets the content of each place's infowindow.
-  */
-  function createMarker(place) {
-    var marker = new google.maps.Marker({
-      map: map,
-      icon: 'http://maps.google.com/mapfiles/ms/icons/blue-dot.png',
-      name: place.name.toLowerCase(),
-      position: place.geometry.location,
-      place_id: place.place_id,
-      animation: google.maps.Animation.DROP
-    });    
-    var address;
-    if (place.vicinity !== undefined) {
-      address = place.vicinity;
-    } else if (place.formatted_address !== undefined) {
-      address = place.formatted_address;
-    }       
-    var contentString = '<div style="font-weight: bold">' + place.name + '</div><div>' + address + '</div>' + self.foursquareInfo ;
-
-    google.maps.event.addListener(marker, 'click', function() {      
-      infowindow.setContent(contentString);      
-      infowindow.open(map, this);
-      map.panTo(marker.position); 
-      marker.setAnimation(google.maps.Animation.BOUNCE);
-      setTimeout(function(){marker.setAnimation(null);}, 1450);
-    });
-
-    markersArray.push(marker);
-    return marker;
-  };
 
   /*
   called after a search, this function clears any markers in the markersArray so that we can start with fresh map with new markers.
@@ -217,10 +227,10 @@ function appViewModel() {
      markersArray[i].setMap(null);
     }
     markersArray.length = 0;
-  };  
+  } 
 
   google.maps.event.addDomListener(window, 'load', initialize);
-};
+}
 
 $(function(){
 ko.applyBindings(new appViewModel());
